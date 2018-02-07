d6ea7fdcbb1af82835d5820160b9d44544c9e629
Unknown <daviola.menezes@gmail.com>
2018-02-07 18:31:46 -0200

    new file to manage auto field by model

---
+++ src/model/DBCombo.class.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Dvi\Adianti\Widget\Form;
+use Adianti\Database\TRecord;
+use Dvi\Adianti\Model\DBFormField;
+
+/**
+ * Model DBCombo
+ *
+ * @version    Dvi 1.0
+ * @package    Model
+ * @subpackage Component
+ * @author     Davi Menezes
+ * @copyright  Copyright (c) 2017. (davimenezes.dev@gmail.com)
+ * @link https://github.com/DaviMenezes
+ */
+class DBCombo extends DBFormField
+{
+    private $value;
+    private $criteria;
+    private $model;
+
+    public function __construct(string $name, string $model, string $value, string $type, bool $required = false, string $label = null)
+    {
+        parent::__construct($name, $type, $required, $label);
+
+        $this->field = new DCombo($name, $label ?? $name, $required);
+
+        $this->model = $model;
+        $this->value = $value;
+    }
+
+    public function setCriteria($criteria)
+    {
+        $this->criteria = $criteria;
+        return $this;
+    }
+
+    public function get()
+    {
+        /**@var TRecord $model*/
+        $items = $this->model::getIndexedArray('id', $this->value, $this->criteria);
+        $this->field->items($items);
+    }
+}
