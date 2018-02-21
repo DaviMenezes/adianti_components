08f5fb35a26f27992f92350c5fef4baed87d091c
DaviMenezes <daviola.menezes@gmail.com>
2018-02-21 03:29:00 -0300

    improvement, refactoring

--- src/model/form/fields/FieldCombo.php
+++ src/model/form/fields/FieldCombo.php
@@ -3,6 +3,7 @@
 namespace Dvi\Adianti\Component\Model\Form\Fields;
 use Adianti\Base\Lib\Database\TRecord;
 use Dvi\Adianti\Model\DBFormField;
+use Dvi\Adianti\Widget\Form\DCombo;
 
 /**
  * Model DBCombo
@@ -20,14 +21,19 @@ class FieldCombo extends DBFormField
     private $criteria;
     private $model;
 
-    public function __construct(string $name, string $model, string $value, string $type, bool $required = false, string $label = null)
+    public function __construct(string $name, string $type, bool $required = false, string $label = null)
     {
         parent::__construct($name, $type, $required, $label);
 
         $this->field = new DCombo($name, $label ?? $name, $required);
+    }
 
+    public function model(string $model, string $value)
+    {
         $this->model = $model;
         $this->value = $value;
+
+        return $this;
     }
 
     public function setCriteria($criteria)
@@ -38,8 +44,15 @@ class FieldCombo extends DBFormField
 
     public function get()
     {
-        /**@var TRecord $model*/
-        $items = $this->model::getIndexedArray('id', $this->value, $this->criteria);
-        $this->field->items($items);
+        if ($this->model) {
+            /**@var TRecord $model*/
+            $items = $this->model::getIndexedArray('id', $this->value, $this->criteria);
+            $this->field->items($items);
+        }
+    }
+
+    public function getField(): DCombo
+    {
+        return $this->field;
     }
 }
