33af1fca3f89aa25c535a795cb66e39c9bdd66c9
DaviMenezes <daviola.menezes@gmail.com>
2018-04-17 20:27:21 -0300

    remove type in action param

--- src/widget/form/DviPanelGroup.php
+++ src/widget/form/DviPanelGroup.php
@@ -446,7 +446,7 @@ class DviPanelGroup implements IDviWidget
         return $this;
     }
 
-    public function addActionBackLink(array $action = null):DActionLink
+    public function addActionBackLink($action = null):DActionLink
     {
         $btn = new DActionLink($action, _t('Back'), 'fa:arrow-left fa-2x');
         $btn->class = 'btn btn-default';
