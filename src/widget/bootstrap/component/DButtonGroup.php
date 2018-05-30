76091fa74db51c52588b36b39d480efc03b192fe
Unknown <daviola.menezes@gmail.com>
2018-05-30 04:07:08 -0300

    improvements in button group

--- src/widget/bootstrap/component/DButtonGroup.php
+++ src/widget/bootstrap/component/DButtonGroup.php
@@ -2,8 +2,8 @@
 namespace Dvi\Adianti\Widget\Bootstrap\Component;
 
 use Adianti\Base\Lib\Widget\Base\TElement;
-use Dvi\Adianti\Route;
 use Dvi\Adianti\Widget\IDviWidget;
+use Dvi\Adianti\WidgetBootstrap\Component\GroupActions;
 
 /**
  * Model DButtonGroup
@@ -15,77 +15,39 @@ use Dvi\Adianti\Widget\IDviWidget;
  * @copyright  Copyright (c) 2017. (davimenezes.dev@gmail.com)
  * @link https://github.com/DaviMenezes
  */
-class DButtonGroup implements IDviWidget
+class DButtonGroup extends GroupActions implements IDviWidget
 {
-    private $items = array();
-    private $style;
+    protected $style;
+    protected $form_default;
+    public $current_group;
 
-    public function add($form_name, array $action, $icon = null, array $parameters = null, $label = null, $style = null)
+    private $group;
+
+    public function __construct($default_form = null)
     {
-        $params ='';
-        if ($parameters) {
-            foreach ($parameters as $key => $value) {
-                $params .= '&'.$key.'='.$value;
-            }
-        }
-        $style = 'style="'.$style.'"';
-        $class = Route::getClassName(get_class($action[0]));
-        $function_post_data = '__adianti_post_data(\''.$form_name.'\', \'class='. $class .'&method='.$action[1].'\');';
-        $onclick_btn = 'onclick="Adianti.waitMessage = \'Carregando\'; '.$function_post_data.' return false;";';
-        $btn = '<button class="btn btn-default dvi_btn '.$style.' '.$onclick_btn.'>';
-        if ($icon) {
-            $icon = str_replace(':', '-', $icon);
-            $btn .= '<li class="fa '.$icon.'"></li>';
-        }
-        $btn .= ($icon ? ' ': '').$label;
-        $btn .= '</button>';
-
-        $this->items[] = $btn;
+        $this->form_default = $default_form;
+        parent::__construct();
     }
 
-    public function addLink(array $action, $icon = null, array $parameters = null, $label = null, $style = null)
+    public function addGroup($action_header = null, $title_group_action = null): DActionGroup
     {
-        $params ='';
-        if ($parameters) {
-            foreach ($parameters as $key => $value) {
-                $params .= '&'.$key.'='.$value;
-            }
-        }
-
-        $style = 'style="'.$style.'"';
-        $class_name = Route::getClassName(get_class($action[0]));
-
-        $onclick_link = 'onclick="Adianti.waitMessage = \'Carregando\'; return false;";';
-        $href = 'href="index.php?class='.$class_name.'&method='.$action[1].$params. '"';
-        $class = 'class="btn btn-default " generator="adianti"';
-
-        $link = '<a '.$href.' '.$class.' '.$style.' '.$onclick_link.'>';
-        if ($icon) {
-            $icon = str_replace(':', '-', $icon);
-            $link .= '<li class="fa '.$icon.'" style="vertical-align:-webkit-baseline-middle;"></li>';
-        }
-        $link .= ($icon ? ' ': '').$label;
-        $link .= '</a>';
-
-        $this->items[] = $link;
-    }
-
-    public function setStyle($style)
-    {
-        $this->style = $style;
+        $this->group = new DActionGroup($action_header, $title_group_action);
+        $this->items[] = $this->group;
+        return $this->group;
     }
 
     public function show()
     {
         $group = new TElement('div');
-        $group->class= 'btn-group';
+        $group->class= ' btn-group';
         $group->role ="group";
         $group->{'aria-label'}="...";
         $group->style = $this->style;
+
         foreach ($this->items as $item) {
             $group->add($item);
         }
 
-        $group->show();
+        return $group->show();
     }
 }
