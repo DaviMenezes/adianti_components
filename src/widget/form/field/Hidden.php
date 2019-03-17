5a74c8bd1be5e23d4a0e98159eaff6696bcc1d0f
DaviMenezes <davimenezes.dev@gmail.com>
2019-03-17 16:05:05 -0300

    ciado metodo showView responsavel por carregar a view do  comp Hidden

--- src/widget/form/field/Hidden.php
+++ src/widget/form/field/Hidden.php
@@ -27,4 +27,34 @@ class Hidden extends THidden
 
         $this->setType(new FieldTypeString());
     }
+
+    public function showView()
+    {
+        // set the tag properties
+        $this->tag->{'id'}     = 'thidden_' . mt_rand(1000000000, 1999999999);
+        $this->tag->{'name'}   = $this->name;  // tag name
+        $this->tag->{'value'}  = $this->value; // tag value
+        $this->tag->{'type'}   = 'hidden';     // input type
+        $this->tag->{'widget'} = 'thidden';
+        $this->tag->{'style'}  = "width:{$this->size}";
+
+        $properties = $this->tag->getProperties();
+
+        $data = [];
+        foreach ($properties as $property => $value) {
+            if (empty($value)) {
+                continue;
+            }
+            $data[$property] = $value;
+        }
+
+        $params = [
+            'label' => $this->error_msg ? $this->wrapperStringClass('verifique') : $this->getLabel(),
+            'field_info' => $this->getFieldInfoValidationErrorData($this->getLabel()),
+            'properties' => $data
+        ];
+        view("form/fields/hidden", $params);
+    }
+
+
 }
