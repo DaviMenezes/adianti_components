6be25ce219fc99ffc9370d6da66371462fb05242
DaviMenezes <davimenezes.dev@gmail.com>
2019-03-17 16:02:00 -0300

    criando metodo showView responsavel por carregar a view do componente

--- src/widget/form/field/Combo.php
+++ src/widget/form/field/Combo.php
@@ -3,9 +3,12 @@
 namespace Dvi\Adianti\Widget\Form\Field;
 
 use Adianti\Base\Lib\Core\AdiantiCoreTranslator;
+use Adianti\Base\Lib\Widget\Base\TScript;
 use Adianti\Base\Lib\Widget\Form\TCombo;
+use Adianti\Base\Lib\Widget\Form\TForm;
 use Dvi\Adianti\Widget\Form\Field\Contract\FormField;
 use Dvi\Adianti\Widget\Form\Field\FormField as FormFieldTrait;
+use Exception;
 
 /**
  *  Combo
@@ -66,6 +69,72 @@ class Combo extends TCombo implements FormField
         }
         return $placeholder;
     }
+
+    public function showView()
+    {
+        // define the tag properties
+        $data['name']  = $this->name;    // tag name
+
+        if ($this->id and empty($this->tag->{'id'})) {
+            $data['id'] = $this->id;
+        }
+
+        if (!empty($this->size)) {
+            if (strstr($this->size, '%') !== false) {
+                $data['style'] = "width:{$this->size};"; //aggregate style info
+            } else {
+                $data['style'] = "width:{$this->size}px;"; //aggregate style info
+            }
+        }
+
+        if (isset($this->changeAction)) {
+            if (!TForm::getFormByName($this->formName) instanceof TForm) {
+                throw new Exception(AdiantiCoreTranslator::translate('You must pass the ^1 (^2) as a parameter to ^3', __CLASS__, $this->name, 'TForm::setFields()'));
+            }
+
+            $string_action = $this->changeAction->serialize(false);
+            $data['changeaction'] = "__adianti_post_lookup('{$this->formName}', '{$string_action}', '{$this->id}', 'callback')";
+            $data['onChange'] = $this->getProperty('changeaction');
+        }
+
+        if (isset($this->changeFunction)) {
+            $data['changeaction'] = $this->changeFunction;
+            $data['onChange'] = $this->changeFunction;
+        }
+
+        // verify whether the widget is editable
+        if (!parent::getEditable()) {
+            // make the widget read-only
+            $data['onclick']  = "return false;";
+            $data['style']   .= ';pointer-events:none';
+            $data['tabindex'] = '-1';
+            $data['class']    = 'tcombo_disabled'; // CSS
+        }
+
+        if ($this->searchable) {
+            $data['role'] = 'tcombosearch';
+        }
+
+        // shows the combobox
+//        $this->renderItems();
+
+        $params = [
+            'label' => $this->error_msg ? $this->wrapperStringClass('verifique') : $this->getLabel(),
+            'field_info' => $this->getFieldInfoValidationErrorData($this->getLabel()),
+            'properties' => $data,
+            'option_items' => $this->items,
+            'options_properties' => []
+        ];
+
+        view('form/fields/combo', $params);
+
+        if ($this->searchable) {
+            $select = $this->getTextPlaceholder();
+            TScript::create("tcombo_enable_search('#{$this->id}', '{$select}')");
+
+            if (!parent::getEditable()) {
+                TScript::create(" tmultisearch_disable_field( '{$this->formName}', '{$this->name}'); ");
+            }
+        }
+    }
 }
-
-
